e4fb5c5d8ec928b4d2a7ef60de936e65606078e8
Madhav Khating <119400880+madhavkhating@users.noreply.github.com>
2024-07-06 15:00:05 +0530

    Update1 main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
+tags = {
+Name =var.cloud
 }
 resource "aws_iam_user" "iamusr1" {
   name = var.iam
   path = "/system/"
+tags = {
+Name =var.cloud
+}
 }
 resource "aws_instance" "myins1" {
   ami           = "ami-01b799c439fd5516a"
@@ -15,6 +20,9 @@ resource "aws_instance" "myins1" {
 }
 resource "aws_s3_bucket" "mybkt" {
   bucket = var.bkt
+tags = {
+Name =var.cloud
+}
 }
 resource "aws_subnet" "sbnet1" {
   vpc_id     = aws_vpc.myvpc.id
@@ -23,4 +31,10 @@ resource "aws_subnet" "sbnet1" {
   tags = {
     Name = "sbnet1"
   }
+tags = {
+Name =var.cloud
+}
+}
+varibale "cloud" {
+default = "cloud"
 }
